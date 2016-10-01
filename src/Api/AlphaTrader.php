fe23bb20f61f06ae6baa92094ba5c65e12635299
Tr0nYx <tronyx@bric.finance>
2016-10-01 21:05:22 +0200

    added VotingController

--- src/Api/AlphaTrader.php
+++ src/Api/AlphaTrader.php
@@ -530,4 +530,89 @@ class AlphaTrader extends AbstractAlphaTrader
             $counterparty
         );
     }
+
+    /**
+     * @return \Doctrine\Common\Collections\ArrayCollection
+     */
+    public function getMyPolls()
+    {
+        return $this->getVotingController()->getMyPolls();
+    }
+
+    /**
+     * @return \Doctrine\Common\Collections\ArrayCollection
+     */
+    public function getAllKnownPolls()
+    {
+        return $this->getVotingController()->getAllKnownPolls();
+    }
+
+    /**
+     * @param Company $company
+     *
+     * @return \Alphatrader\ApiBundle\Model\AbstractPoll
+     */
+    public function setCompanyCashoutPoll(Company $company)
+    {
+        return $this->getVotingController()->setCompanyCashoutPoll($company);
+    }
+
+    /**
+     * @param Company $company
+     * @param         $dailyWage
+     *
+     * @return \Alphatrader\ApiBundle\Model\AbstractPoll
+     */
+    public function setCompanyEmployCeo(Company $company, $dailyWage)
+    {
+        return $this->getVotingController()->setCompanyEmployCeo($company,$dailyWage);
+    }
+
+    /**
+     * @param Company $company
+     *
+     * @return \Alphatrader\ApiBundle\Model\AbstractPoll
+     */
+    public function setCompanyLiquidation(Company $company)
+    {
+        return $this->getVotingController()->setCompanyLiquidation($company);
+    }
+
+    /**
+     * @param $pollid
+     *
+     * @return \Alphatrader\ApiBundle\Model\AbstractPoll
+     */
+    public function getPoll($pollid)
+    {
+        return $this->getVotingController()->getPoll($pollid);
+    }
+
+    /**
+     * @param $pollid
+     */
+    public function setExecutePoll($pollid)
+    {
+        $this->getVotingController()->executePoll($pollid);
+    }
+
+    /**
+     * @param $pollid
+     */
+    public function setdeletePoll($pollid)
+    {
+        $this->getVotingController()->deletePoll($pollid);
+    }
+
+    /**
+     * @param $pollid
+     * @param $voices
+     * @param $votingType
+     *
+     * @return AbstractPoll
+     */
+    public function setvotePoll($pollid, $voices, $votingType)
+    {
+        return $this->getVotingController()->votePoll($pollid, $voices, $votingType);
+    }
 }
