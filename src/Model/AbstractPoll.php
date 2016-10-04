6f0f3d9b9bf0544d29b5d9f9d5062d9595bd2dbd
Tr0nYx <tronyx@bric.finance>
2016-10-04 22:59:51 +0200

    AbstractPoll

--- src/Model/AbstractPoll.php
+++ src/Model/AbstractPoll.php
@@ -11,5 +11,350 @@ use JMS\Serializer\Annotation;
  */
 class AbstractPoll
 {
-    use PollTrait;
+    use DateTrait;
+
+    /**
+     * @var string
+     * @Annotation\Type("string")
+     * @Annotation\SerializedName("id")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @Annotation\Type("string")
+     * @Annotation\SerializedName("abstentionRule")
+     */
+    private $abstentionRule;
+
+    /**
+     * @var float
+     * @Annotation\Type("float")
+     * @Annotation\SerializedName("castVotesPercentage")
+     */
+    private $castVotesPercentage;
+
+    /**
+     * @var VoiceNumber[]
+     * @Annotation\Type("ArrayCollection<Alphatrader\ApiBundle\Model\VoiceNumber>")
+     * @Annotation\SerializedName("group")
+     */
+    private $group;
+
+    /**
+     * @var string
+     * @Annotation\Type("string")
+     * @Annotation\SerializedName("motion")
+     */
+    private $motion;
+
+    /**
+     * @var UserName
+     * @Annotation\Type("Alphatrader\ApiBundle\Model\UserName")
+     * @Annotation\SerializedName("pollInitiator")
+     */
+    private $pollInitiator;
+
+    /**
+     * @var integer
+     * @Annotation\Type("integer")
+     * @Annotation\SerializedName("resultExpireDate")
+     */
+    private $resultExpireDate;
+
+    /**
+     * @var int
+     * @Annotation\Type("integer")
+     * @Annotation\SerializedName("totalNumberOfCastVotes")
+     */
+    private $totalNumberOfVotes;
+
+    /**
+     * @var int
+     * @Annotation\Type("integer")
+     * @Annotation\SerializedName("totalNumberOfVoices")
+     */
+    private $totalNumberOfVoices;
+
+    /**
+     * @var string
+     * @Annotation\Type("string")
+     * @Annotation\SerializedName("type")
+     */
+    private $type;
+
+    /**
+     * @var Vote[]
+     * @Annotation\Type("ArrayCollection<Alphatrader\ApiBundle\Model\Vote>")
+     * @Annotation\SerializedName("votes")
+     */
+    private $votes;
+
+    /**
+     * @var float
+     * @Annotation\Type("float")
+     * @Annotation\SerializedName("approvalVotesPercentage")
+     */
+    private $approvVotesPercent;
+
+    /**
+     * @var CompanyName
+     * @Annotation\Type("Alphatrader\ApiBundle\Model\CompanyName")
+     * @Annotation\SerializedName("company")
+     */
+    private $company;
+
+    /**
+     * @var UserName
+     * @Annotation\Type("Alphatrader\ApiBundle\Model\UserName")
+     * @Annotation\SerializedName("applicant")
+     */
+    protected $applicant;
+
+    /**
+     * @var float
+     * @Annotation\Type("float")
+     * @Annotation\SerializedName("dailyWage")
+     */
+    private $dailyWage;
+    
+    /**
+     * @return string
+     */
+    public function getAbstentionRule()
+    {
+        return $this->abstentionRule;
+    }
+
+    /**
+     * @param string $abstentionRule
+     */
+    public function setAbstentionRule($abstentionRule)
+    {
+        $this->abstentionRule = $abstentionRule;
+    }
+
+    /**
+     * @return float
+     */
+    public function getCastVotesPercentage()
+    {
+        return $this->castVotesPercentage;
+    }
+
+    /**
+     * @param float $castVotesPercentage
+     */
+    public function setCastVotesPercentage($castVotesPercentage)
+    {
+        $this->castVotesPercentage = $castVotesPercentage;
+    }
+
+    /**
+     * @return VoiceNumber[]
+     */
+    public function getGroup()
+    {
+        return $this->group;
+    }
+
+    /**
+     * @param VoiceNumber[] $group
+     */
+    public function setGroup($group)
+    {
+        $this->group = $group;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMotion()
+    {
+        return $this->motion;
+    }
+
+    /**
+     * @param string $motion
+     */
+    public function setMotion($motion)
+    {
+        $this->motion = $motion;
+    }
+
+    /**
+     * @return UserName
+     */
+    public function getPollInitiator()
+    {
+        return $this->pollInitiator;
+    }
+
+    /**
+     * @param UserName $pollInitiator
+     */
+    public function setPollInitiator($pollInitiator)
+    {
+        $this->pollInitiator = $pollInitiator;
+    }
+
+    /**
+     * @return int
+     */
+    public function getResultExpireDate()
+    {
+        return $this->resultExpireDate;
+    }
+
+    /**
+     * @param int $resultExpireDate
+     */
+    public function setResultExpireDate($resultExpireDate)
+    {
+        $this->resultExpireDate = $resultExpireDate;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTotalNumberOfCastVotes()
+    {
+        return $this->totalNumberOfVotes;
+    }
+
+    /**
+     * @param int $totalNumberOfVotes
+     */
+    public function setTotalNumberOfCastVotes($totalNumberOfVotes)
+    {
+        $this->totalNumberOfVotes = $totalNumberOfVotes;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTotalNumberOfVoices()
+    {
+        return $this->totalNumberOfVoices;
+    }
+
+    /**
+     * @param int $totalNumberOfVoices
+     */
+    public function setTotalNumberOfVoices($totalNumberOfVoices)
+    {
+        $this->totalNumberOfVoices = $totalNumberOfVoices;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return Vote[]
+     */
+    public function getVotes()
+    {
+        return $this->votes;
+    }
+
+    /**
+     * @param Vote[] $votes
+     */
+    public function setVotes($votes)
+    {
+        $this->votes = $votes;
+    }
+
+    /**
+     * @return float
+     */
+    public function getApprovVotesPercent()
+    {
+        return $this->approvVotesPercent;
+    }
+
+    /**
+     * @param float $approvVotesPercent
+     */
+    public function setApprovVotesPercent($approvVotesPercent)
+    {
+        $this->approvVotesPercent = $approvVotesPercent;
+    }
+
+    /**
+     * @return CompanyName
+     */
+    public function getCompany()
+    {
+        return $this->company;
+    }
+
+    /**
+     * @param CompanyName $company
+     */
+    public function setCompany($company)
+    {
+        $this->company = $company;
+    }
+
+    /**
+     * @return UserName
+     */
+    public function getApplicant()
+    {
+        return $this->applicant;
+    }
+
+    /**
+     * @param UserName $applicant
+     */
+    public function setApplicant($applicant)
+    {
+        $this->applicant = $applicant;
+    }
+
+    /**
+     * @return float
+     */
+    public function getDailyWage()
+    {
+        return $this->dailyWage;
+    }
+
+    /**
+     * @param float $dailyWage
+     */
+    public function setDailyWage($dailyWage)
+    {
+        $this->dailyWage = $dailyWage;
+    }
 }
