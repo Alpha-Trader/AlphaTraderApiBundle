feb90d93c65e024210aba20c90283851f3283bde
Tr0nYx <tronyx@bric.finance>
2016-12-02 12:19:50 +0100

    added FixedIncomeSecurity Model

--- src/Api/ApiClient.php
+++ src/Api/ApiClient.php
@@ -161,7 +161,6 @@ class ApiClient
      * @param                                     $class
      *
      * @return \Alphatrader\ApiBundle\Model\Error|mixed
-     * @throws HttpErrorException
      */
     public function parseResponse(ResponseInterface $request, $class)
     {
@@ -176,7 +175,7 @@ class ApiClient
                 'json'
             );
 
-            throw new AlphaTraderApiException($request->getStatusCode(), $oResult);
+            //throw new AlphaTraderApiException($request->getStatusCode(), $oResult);
         }
         return $oResult;
     }
