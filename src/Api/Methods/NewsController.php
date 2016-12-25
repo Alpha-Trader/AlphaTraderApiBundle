a0637f5f07f02a05f0f530b76f398233716bd124
Leon Bergmann <leon.bergmann@sky-lab.de>
2016-12-25 13:00:52 +0100

    added NewsController & improved PostController

---
+++ src/Api/Methods/NewsController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Alphatrader\ApiBundle\Api\Methods;
+
+use Alphatrader\ApiBundle\Api\ApiClient;
+
+/**
+ * Class NewsController
+ * @package Api\Methods
+ * @author ljbergmann <l.bergmann@sky-lab.de>
+ */
+class NewsController extends ApiClient
+{
+    /**
+     * @param $count
+     * @return \Alphatrader\ApiBundle\Model\Error|\Alphatrader\ApiBundle\Model\Posts[]
+     */
+    public function getHotNews($count)
+    {
+        $data = $this->post('/v2/news/hot', ['count' => $count]);
+        return $this->parseResponse($data, 'ArrayCollection<Alphatrader\ApiBundle\Model\Posts>');
+    }
+
+    /**
+     * @param $postId
+     * @return \Alphatrader\ApiBundle\Model\Error|mixed
+     */
+    public function getPost($postId)
+    {
+        $data = $this->post('/v2/news/'.$postId);
+        return $this->parseResponse($data, 'Alphatrader\ApiBundle\Model\Posts');
+    }
+}
